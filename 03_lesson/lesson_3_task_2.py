8bf9358f70df4031c6ec51d522638e26f7a235a4
Сергей Кожемякин <sergey.kozhemyakin@gmail.com>
2026-01-30 12:42:29 +0200

    Третья  Д.З ,исправлен  файл smartphone.py

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,9 +1,13 @@
 from smartphone import Smartphone
+catalog = [
+Smartphone("Nokia", "3310", "89213423455"),
+Smartphone("Moto", "1122", "89991234543" ),
+Smartphone("Iphone","13", "89227896534"),
+Smartphone("Samsung","s12", "89773451222"),
+Smartphone("Fly", "1012", "89435674433")]
 
-first = Smartphone("Nokia", "3310", "89213423455")
-second = Smartphone("Moto", "1122", "89991234543" )
-third = Smartphone("Iphone", "13", "89227896534")
-fourth = Smartphone("Samsung", "s12", "89773451222")
-five = Smartphone("Fly", "1012", "89435674433")
-catalog = [first, second, third, fourth, five]
-print(f" {first}\n {second}\n {third}\n {fourth}\n {five}")
+for smartphone in catalog:
+    print(f"{smartphone.brand} - {smartphone.model}. {smartphone.phone_number}")
+
+
+
