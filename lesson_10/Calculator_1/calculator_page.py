fe07eb1ad16844b8489fdd90d5304425f0b47af9
Сергей Кожемякин <sergey.kozhemyakin@gmail.com>
2026-04-03 21:53:37 +0200

    Д.З номер 10 Отчёт линтера в файле lint.report_lesson_10.txt

--- lesson_10/Calculator_1/calculator_page.py
+++ lesson_10/Calculator_1/calculator_page.py
@@ -1,30 +1,52 @@
 import allure
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as es
 
 
 class CalculatorPage:
+    """
+    Класс для работы со страницей медленного калькулятора.
+    """
 
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver) -> None:
+        """
+        Инициализация страницы калькулятора.
+        :param driver: Экземпляр WebDriver.
+        """
         self.driver = driver
-        self.driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+        self.driver.get(url=
+            "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+        )
 
-    @allure.title("Установка задержки таймера на 45 секунд")
-    def input_delay(self):
+    @allure.step("Установка задержки таймера на 45 секунд")
+    def input_delay(self) -> None:
+        """
+        Устанавливает значение задержки в поле ввода.
+        :return: None
+        """
         input_field = self.driver.find_element(By.CSS_SELECTOR, "#delay")
         input_field.clear()
         input_field.send_keys("45")
 
-    @allure.title("Нажатие клавиш 7, +, 8, и  = на калькуляторе ")
-    def buttons_of_calc(self):
+    @allure.step("Нажатие клавиш 7, +, 8, и  = на калькуляторе ")
+    def buttons_of_calc(self) -> None:
+        """
+        Последовательно нажимает кнопки на панели калькулятора.
+        :return: None
+        """
         buttons = ["7", "+", "8", "="]
         for btn in buttons:
             self.driver.find_element(By.XPATH, f'//span[text()="{btn}"]').click()
 
-    @allure.title("Вывод результата вычисления ")
-    def get_result(self):
+    @allure.step("Вывод результата вычисления ")
+    def get_result(self) -> str:
+        """
+        Ожидает появления результата и возвращает текст с экрана калькулятора.
+        :return: Строковое значение результата (например, "15").
+        """
         wait = WebDriverWait(self.driver, 46)
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15"))
+        wait.until(es.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15"))
         result_text = self.driver.find_element(By.CSS_SELECTOR, ".screen").text
         return result_text
