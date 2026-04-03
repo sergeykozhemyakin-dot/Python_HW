538cf76f74528806add4b2186b8e71859df972a9
Сергей Кожемякин <sergey.kozhemyakin@gmail.com>
2026-04-03 17:32:44 +0200

    Д.З номер 10 Отчёт линтера в файле lint.report.txt

---
+++ lesson_10/Store_1/login_page.py
@@ -0,0 +1,29 @@
+import allure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@allure.feature("Личный кабинет пользователя")
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.get("https://www.saucedemo.com")
+        self.wait = WebDriverWait(driver, 10)
+
+    @allure.story("Авторизация")
+    @allure.severity(allure.severity_level.BLOCKER)
+    @allure.title("Вход в систему под пользователем { user_name}")
+    def login(self, user_name=str, password=str) -> None:
+        with allure.step("Ввод логина"):
+            login_field = self.wait.until(EC.presence_of_element_located((By.ID, "user-name")))
+            login_field.clear()
+            login_field.send_keys(user_name)
+
+            with allure.step("Ввод пароля"):
+                password_field = self.driver.find_element(By.ID, "password")
+                password_field.clear()
+                password_field.send_keys(password)
+
+            with allure.step("Нажатие на кнопку ввод"):
+                self.driver.find_element(By.ID, "login-button").click()
